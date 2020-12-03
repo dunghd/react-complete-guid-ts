e4f445b9f2666403c93f878d6581802f64a97680
Dung Ho <ddh@elca.vn>
2020-12-03 19:07:51 +0700

    Class base typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person, { IPersonProps } from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface IAppProps {
+}
+
+interface IAppState {
+  persons: Array<IPersonProps>;
+  otherState: string
+}
+
+class App extends Component<IAppProps, IAppState> {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ] as IPersonProps[],
+    otherState: 'some other value'
+  } as IAppState;
+
+  switchNameHandler = (newName: string) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 },
+      ] as IPersonProps[]
+    });
+  };
+
+  nameChangedHandler = (event: any) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 27 },
+      ] as IPersonProps[]
+    });
+  };
+
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm React App</h1>
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler(`DDH!!!`)}
+        > Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'DDH TEST 123')}
+          changed={this.nameChangedHandler}
+        >TEST TEST TEST</Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Person/Person.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Person.css';
+
+export interface IPersonProps {
+  name: string,
+  age: number,
+  click?: (event: any) => void,
+  changed?: (event: any) => void,
+  children?: React.ReactNode
+};
+
+const person: React.FC<IPersonProps> = (props: IPersonProps) => {
+  return (
+    <div className="Person">
+      <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name}></input>
+    </div>
+  );
+}
+
+export default person;
