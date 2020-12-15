a7e182f7786442761a3710f545d16078bd3f1424
Dung Ho <ddh@elca.vn>
2020-12-15 18:34:56 +0700

    75. More on CSS Modules

--- src/Person/Person.tsx
+++ src/Person/Person.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
-
-import './Person.css';
+import classes from './Person.module.css';
 
 export interface IPersonProps {
   id: string,
@@ -12,36 +10,15 @@ export interface IPersonProps {
   children?: React.ReactNode
 };
 
-const StyleDiv = styled.div`
-  width: 60%;
-  margin: 16px auto;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 16px;
-  text-align: center;
-
-  @media (min-width: 500px)  {
-        width: 450px;
-    }
-  `;
-
 const person: React.FC<IPersonProps> = (props: IPersonProps) => {
-  const style = {
-    '@media (min-width: 600px)': {
-      width: '450px',
-      backgroundColor: 'red'
-    }
-  };
-
   return (
-    // <div className="Person" style={style}>
-    <StyleDiv>
+    <div className={classes.Person}>
       <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old!
       </p>
       <p> {props.children}</p>
       <input type="text" onChange={props.changed} value={props.name}></input>
-    </StyleDiv >
+    </div >
   );
 }
 
